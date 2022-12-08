15af5cb888d61ae738122d374da32ad66284df94
jprzygodaGD <jprzygoda@griddynamics.com>
2022-12-08 11:39:34 +0100

    Solution for module 7 added

--- practice/7_concurrency/task1_fibonacci/template.py
+++ practice/7_concurrency/task1_fibonacci/template.py
@@ -1,6 +1,9 @@
 import os
+import datetime
 from random import randint
-
+import sys
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
+sys.set_int_max_str_digits(100000000)
 
 OUTPUT_DIR = './output'
 RESULT_FILE = './output/result.csv'
@@ -12,20 +15,54 @@ def fib(n: int):
     f0, f1 = 0, 1
     for _ in range(n-1):
         f0, f1 = f1, f0 + f1
-    return f1
+    return f1, n
+
+
+def func1_helper(number: int, value: int):
+    """Writes value of Fibonacci sequence to file."""
+
+    with open(f"{OUTPUT_DIR}/file{number}.txt", "w") as opened_file:
+        opened_file.write(str(value))
 
 
 def func1(array: list):
-    pass
+    """ Calculates value of Fibonacci sequence using multiprocessing. """
+
+    with ProcessPoolExecutor(16) as executor:
+        futures = [executor.submit(fib, number) for number in array]
+        for future in as_completed(futures):
+            value, number = future.result()
+            func1_helper(number, value)
+
+
+def func2_helper(file):
+    """ Reads file contents. """
+    ordinal_num = file.split('.')[0].split('e')[1]
+    file_path = os.path.join(OUTPUT_DIR, file)
+    with open(file_path) as opened_file:
+        line = opened_file.readline()
+        to_write = ordinal_num + ',' + line + '\n'
+        return to_write
 
 
 def func2(result_file: str):
-    pass
+    """ Writes contents to the resul file. """
+
+    with ThreadPoolExecutor() as executor:
+        with open(result_file, "w") as result_file:
+            futures = [executor.submit(func2_helper, file) for file in sorted(os.listdir(OUTPUT_DIR))]
+            for future in as_completed(futures):
+                text = future.result()
+                result_file.write(text)
 
 
 if __name__ == '__main__':
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
+    # It took around 40 seconds to complete without any concurrency techniques
+    start = datetime.datetime.now()
     func1(array=[randint(1000, 100000) for _ in range(1000)])
     func2(result_file=RESULT_FILE)
+    end = datetime.datetime.now() - start
+    print(end)
--- practice/7_concurrency/task2_apod_api/template.py
+++ practice/7_concurrency/task2_apod_api/template.py
@@ -1,24 +1,53 @@
-API_KEY = "YOUR_KEY"
+import requests
+import os
+import datetime
+import queue
+from threading import Thread, Event
+
+API_KEY = "L5ey8AkzCMooSJGgyY3VW3kiwFFesJGEbYrSg1N4"
 APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
 OUTPUT_IMAGES = './output'
 
 
-def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
-    pass
+def get_apod_metadata_producer(buffer, event, start_date: str, end_date: str, api_key: str):
+
+    url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}&start_date={start_date}&end_date={end_date}'
+    response = requests.get(url)
+    for data in response.json():
+        if data['media_type'] == 'image':
+            buffer.put(data['hdurl'])
+    event.set()
 
 
-def download_apod_images(metadata: list):
-    pass
+def download_apod_images_consumer(buffer, event, ident):
+
+    while not event.is_set() or not buffer.empty():
+        item = buffer.get()
+        file_name = str(item).split('/')[-1].split('.')[0]
+        with open(f'{OUTPUT_IMAGES}/{file_name}.jpg', 'wb') as handler:
+            img_data = requests.get(item)
+            handler.write(img_data.content)
 
 
 def main():
-    metadata = get_apod_metadata(
-        start_date='2021-08-01',
-        end_date='2021-09-30',
-        api_key=API_KEY,
-    )
-    download_apod_images(metadata=metadata)
+    if not os.path.exists(OUTPUT_IMAGES):
+        os.makedirs(OUTPUT_IMAGES)
+
+    q = queue.Queue()
+    event = Event()
+    producer_thread = Thread(target=get_apod_metadata_producer, args=(q, event, '2021-08-01', '2021-09-30', API_KEY))
+    consumer_threads = [Thread(target=download_apod_images_consumer, args=(q, event, ident)) for ident in range(54)]
+    for consumer in consumer_threads:
+        consumer.start()
+    producer_thread.start()
+    producer_thread.join()
+    for consumer in consumer_threads:
+        consumer.join()
 
 
 if __name__ == '__main__':
+    # Sequentially code took: 3 min 11 sec
+    start = datetime.datetime.now()
     main()
+    end = datetime.datetime.now() - start
+    print(end)
